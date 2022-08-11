fa30e1364fb042743852a984364fd4340a7320c5
Roman Krylov <0087897@gmail.com>
2022-08-11 13:06:43 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,94 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        /*
+Задание 1. Написать цикл, который будет выводить числа от 0 до 10
+        for (int i = 0; i <= 10; i = i + 1) {
+            System.out.println(i);
+        }
+    }
+}
+
+         */
+
+/*
+Задание 2. Написать цикл, который будет выводить числа от 5 до 15
+        for (int i = 5; i <= 15; i = i + 1){
+                System.out.println(i);
+        }
+    }
+}
+
+ */
+
+/*Задание 3. Написать цикл, который будет выводить числа от 10 до 0
+
+        for (int i = 10; i >= 0; i = i - 1) {
+            System.out.println(i);
+        }
+    }
+}
+
+ */
+
+/*Задание 4. Написать цикл, который будет выводить числа от 17 до 9
+
+        for (int i = 17; i >= 9; i = i - 1) {
+            System.out.println(i);
+        }
+    }
+}
+
+ */
+
+//Задание 5. Написать цикл, который будет выводить чётные числа от 0 до 10
+
+        for (int i = 0; i <= 10; i = i + 2) {
+            System.out.println(i);
+        }
+    }
+}
+
+
+
+
+ /*
+бесконечный цикл (произведение чисел)
+        for (;;) {
+            System.out.println("Введите первое число:");
+            int value1 = new Scanner(System.in).nextInt();
+            System.out.println("Введите второе число:");
+            int value2 = new Scanner(System.in).nextInt();
+            int result = value1 * value2;
+            System.out.println("Произведение чисел равно: " + result);
+        }
+
+         */
+
+        /* четные числа
+        for (int i = 0; i <= 100; i = i + 2) {
+            System.out.println(i);
+*/
+
+        /* цикл положительный с условием
+        for (int i = 15; i <= 40; i = i + 1) {
+                System.out.println(i);
+
+            if (i == 16) {
+                System.out.println("Вы можете обучаться вождению");
+            }
+
+            if (i == 18) {
+                System.out.println("Вы можете получать водительские права");
+            }
+
+            if (i == 35) {
+                System.out.println("Вы можете баллотироваться на пост президента");
+            }
+        }
+
+        }
+    }
+*/
